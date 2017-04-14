702848e13707e1b6752883fb2324df40083bac2c
ashik <ashik@DESKTOP-EP9DQRO.home>
2017-04-13 22:50:55 -0400

    The constructor now takes in the map of the game itself only

--- src/tron/Player.java
+++ src/tron/Player.java
@@ -27,6 +27,8 @@ class Player {
         		map[i][j] = new Node(i,j);        	
         	}
         }
+        
+        AStar path = new AStar(map);
         	
         // game loop
         while (true) //insert logic for how to move
@@ -210,39 +212,50 @@ class AStar{
 	PriorityQueue<Node> queue = new PriorityQueue<>();
 	ArrayList<Node> explored = new ArrayList<>();
 	Node[][] grid; //flip x and y coordinate
+	ArrayList<Node> path = new ArrayList<>();
 	Node startNode;
 	Node goalNode;
+	int minDistance = Integer.MAX_VALUE;
 	
-	public AStar(Node startNode, Node goalNode, int[][] map) {
+	public AStar(Node[][] map) {
+		grid = map;
+	}
+	
+	@SuppressWarnings("unused")
+	private int getMinDistance(Node startNode, Node goalNode)
+	{
+		if(getPath(startNode, goalNode) != null)
+		{
+			return path.size();
+		}
+		else
+		{
+			return Integer.MAX_VALUE;
+		}
+	}
+	private ArrayList<Node> getPath(Node startNode, Node goalNode)
+	{
 		this.startNode = startNode;
 		this.goalNode = goalNode;
-		grid = new Node[map.length][map.length];
-		
 		//sets up start and goal nodes
 		grid[startNode.getPosX()][startNode.getPosY()] = startNode;
 		grid[goalNode.getPosX()][goalNode.getPosY()] = goalNode;
+		
 		startNode.setDisFromStart(0);
 		startNode.setDisFromGoal(startNode.distanceFrom(goalNode));
 		startNode.setCost();
 		
-		/**
-		 * Creating basic map structure
-		 */
-		//populating grid
-		for(int i = 0 ; i < map.length ; i++)
-		{
-			for(int j = 0 ; j < map[i].length ; j ++)
-			{
-				if(grid[i][j] !=startNode && grid[i][j] != goalNode && map[i][j] == 1) //populates the grid with nodes if the map is indicated with a 1 
-				{
-					grid[i][j] = new Node(i,j);				
-				}
-			}
-		}
-		
 		queue.add(startNode); //will initialize with startNode being added to the queue
 		startNode.setParent(null);
-		printList(grid); //displays the map
+		
+		if(!search())
+		{
+			return null;
+		}
+		else
+		{
+			return path;
+		}
 	}
 	
 	public PriorityQueue<Node> getQueue() {
@@ -276,9 +289,7 @@ class AStar{
 			
 			if(parent.equals(goalNode)) //if the goal is found
 			{
-				printPath(goalNode); //displays the path
-				System.out.println("AStar Search Path Found!");
-//				printExplored(); //for test purposes
+				populatePath(goalNode); //displays the path
 				return true;
 			}
 			else
@@ -295,10 +306,6 @@ class AStar{
 							child.setDisFromStart(parent.getDisFromStart() + 1);
 							child.setDisFromGoal(child.distanceFrom(goalNode));
 							child.setCost();
-//							if(child == grid[7][5]) //for testing certain node when they are being added to the queue
-//							{
-//								System.out.println("tested node: " + child);
-//							}
 							queue.add(child); //adds to queue
 						}
 					}
@@ -309,17 +316,14 @@ class AStar{
 		return false; 
 	}
 
-	private void printPath(Node goal) {
-		int count = 0;
+	private void populatePath(Node goal) {
+		minDistance = 0;
 		while(goal.getParent() != null)
 		{
-			count++;
-			System.out.print(goal + " <--- ");
+			minDistance++;
+			path.add(goal);
 			goal = goal.getParent();
 		}
-		System.out.print(goal);
-		System.out.println();
-		System.out.println("Total of " + count + " Nodes");
 	}
 	
 	private void setNodeChildren(Node n) //probably made this more complicated than it has to
@@ -378,31 +382,6 @@ class AStar{
 			n.addChild(grid[x][y - 1]);
 			n.addChild(grid[x - 1][y]);
 			n.addChild(grid[x + 1][y]);
-		}
-	}
-	
-	private void printExplored()
-	{
-		for(Node n : explored)
-		{
-			System.out.println(n);
-		}
-	}
-	
-	private void printList(Node[][] arr)
-	{
-		for(Node[] a : arr)
-		{
-			for(Node n : a)
-			{
-				if(n != null)
-				{
-					System.out.print("1 ");
-				}
-				else
-					System.out.print("0 ");
-			}
-			System.out.println();
 		}
 	}
 }
